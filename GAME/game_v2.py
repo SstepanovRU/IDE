35eac26086822ef923acd42079c3c48fe6158200
SstepanovRU <sstepanov.ru@gmail.com>
2025-01-26 15:35:54 +0300

    home work v1

--- GAME/game_v2.py
+++ GAME/game_v2.py
@@ -1,12 +1,13 @@
 """Игра угадай число
-Компьютер сам загадывает и сам угадывает число
+Компьютер сам загадывает и сам угадывает число тремя разными алгоритмами
 """
 
 import numpy as np
 
 
 def random_predict(number: int = 1) -> int:
-    """Рандомно угадываем число
+    """Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -21,11 +22,59 @@ def random_predict(number: int = 1) -> int:
         predict_number = np.random.randint(1, 101)  # предполагаемое число
         if number == predict_number:
             break  # выход из цикла если угадали
+
     return count
 
+def game_core_v2(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, а потом уменьшаем
+    или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+
+    return count
+
+def game_core_v3(number: int = 1) -> int:
+    """ Отгадываем число путем сокращения диапазона загаданных чисел
+    путем деления исходного диапазона на два, сокращая диапазон по результатам сравнения
+    
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    low_limit = 1
+    high_limit = 100
+    predict = 0
+    count = 0
+    
+    while number != predict:
+        predict = (low_limit + high_limit) // 2
+        count += 1
+        if number > predict:
+            low_limit = predict + 1        
+        elif number < predict:
+            high_limit = predict - 1
+
+    return count
 
 def score_game(random_predict) -> int:
-    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
@@ -35,16 +84,21 @@ def score_game(random_predict) -> int:
     """
     count_ls = []
     #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
-    return score
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
 
 
-if __name__ == "__main__":
-    # RUN
-    score_game(random_predict)
+#Run benchmarking to score effectiveness of all algorithms
+print('Run benchmarking for random_predict: ', end='')
+score_game(random_predict)
+
+print('Run benchmarking for game_core_v2: ', end='')
+score_game(game_core_v2)
+
+print('Run benchmarking for game_core_v3: ', end='')
+score_game(game_core_v3)
